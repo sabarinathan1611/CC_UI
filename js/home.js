035cff818a5155a7e6d42633864b5f0ef431c44c
sabarinathan1611 <sabarinathan.project@gmail.com>
2024-04-22 23:37:44 +0530

    Nav bar

---
+++ js/home.js
@@ -0,0 +1,11 @@
+const sidbar = document.querySelector(".sidebar");
+
+function showSidebar() {
+  console.log("in");
+  sidbar.style.display = "flex";
+}
+
+function hideSidebar() {
+  console.log("in");
+  sidbar.style.display = "none";
+}
