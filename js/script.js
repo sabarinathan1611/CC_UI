f4c1bd6f8ef075e0fc1310c313feefe35c8bb4f6
sabarinathan1611 <sabarinathan.project@gmail.com>
2024-08-04 16:13:39 +0530

    Profile Page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["File Size (MB)","No of Password"], 
+            datasets: [{
+                label: 'Storage',
+                data: [10,20], 
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 100 
+                }
+            }
+        }
+    });
+});
